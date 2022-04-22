a591b464aacc56b69a7a5a31158c8887859509c1
jcmestti <jcmestti@gmail.com>
2022-04-22 12:19:08 -0300

    The Weather Checker is fully working

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,45 @@
-const cityForm = document.querySelector('form');
+const cityFormUI = document.querySelector('form');
+const cityCardUI = document.querySelector('.card');
+const cityDetailsUI = document.querySelector('.details');
+const cityTimeUI = document.querySelector('img.time');
+const cityIconUI = document.querySelector('.icon img');
 
-const updateUICity = async (cityName) => {
+const updateUI = (data) => {
+    // const cityDetails = data.cityDetails;
+    // const cityWeather = data.cityWeather;
+
+    // destructure properties
+    const {cityDetails, cityWeather} = data;
+
+    // update details template
+    cityDetailsUI.innerHTML = `
+        <h5 class="my-5">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${cityWeather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${cityWeather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>`;
+
+        // update the night/day & icon images
+        const iconSrc = `img/icons/${cityWeather.WeatherIcon}.svg`;
+        cityIconUI.setAttribute('src', iconSrc);
+
+        // let timeSrc = null;
+        // if(cityWeather.IsDayTime) {
+        //     timeSrc = 'img/day.svg';
+        // } else {
+        //     timeSrc = 'img/night.svg';
+        // }
+        let timeSrc = cityWeather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+        cityTimeUI.setAttribute('src', timeSrc);
+
+        // remove the d-none class if present    
+        if(cityCardUI.classList.contains('d-none')) {
+            cityCardUI.classList.remove('d-none');
+        }
+};
+
+const updateCity = async (cityName) => {
     const cityDetails = await getCity(cityName);
     const cityWeather = await getWeather(cityDetails.Key);
 
@@ -8,18 +47,18 @@ const updateUICity = async (cityName) => {
         cityDetails: cityDetails,
         cityWeather: cityWeather
     };
-}
+};
 
-cityForm.addEventListener('submit', e => {
+cityFormUI.addEventListener('submit', e => {
     e.preventDefault();
 
-    //cityForm.city is the value from <input name="city">
-    const cityName = cityForm.city.value.trim();
-    cityForm.reset();
+    //cityFormUI.city is the value from <input name="city">
+    const cityName = cityFormUI.city.value.trim();
+    cityFormUI.reset();
 
     //update the UI with new city
-    updateUICity(cityName)
-        .then(data => console.log(data))
+    updateCity(cityName)
+        .then(data => updateUI(data))
         .catch(err => console.log(err));
 });
 
